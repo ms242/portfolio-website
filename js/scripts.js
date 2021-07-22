ca5c0a016fabf4c242b1e01159b8f801481a1c05
ms242 <82464777+ms242@users.noreply.github.com>
2021-07-21 21:44:34 -0500

    Task 2.3 Draft

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Welcome to Maria's Portfolio Site");
+funtion menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
