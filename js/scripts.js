d9b300dabb620e4becf458c247e9479ee413a5e2
ms242 <82464777+ms242@users.noreply.github.com>
2021-07-25 21:58:40 -0500

    Changes per 7/22 instructor feedback

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 console.log("Welcome to Maria's Portfolio Site");
-funtion menuToggle() {
+function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
