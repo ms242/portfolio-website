81d15186aa06870eae6d353a577edd6ec2888cd0
ms242 <82464777+ms242@users.noreply.github.com>
2021-09-14 10:52:23 -0500

    linting first round

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-console.log("Welcome to Maria's Portfolio Site");
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+console.log("Welcome to Maria's Portfolio Site")
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
